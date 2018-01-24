f983e6dd9c413b4710418cc9ae4a274f3e6746eb
cznno <smeraldo.ash@gmail.com>
2018-01-24 19:36:58 +0800

    add monitor

---
+++ monitor-server/src/main/java/person/cznno/monitor/MonitorApplication.java
@@ -0,0 +1,19 @@
+package person.cznno.monitor;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * Created by cznno
+ * Date: 18-1-24
+ */
+@EnableDiscoveryClient
+@EnableAdminServer
+@SpringBootApplication
+public class MonitorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MonitorApplication.class, args);
+    }
+}
